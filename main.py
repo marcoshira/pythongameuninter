b353085ec9bb0c9c345b55c661ed70776ababf85
Marcos Hirazawa <marcos.hirazawa@thebrooklynbrothers.com>
2025-06-24 21:50:12 -0300

    added code and assets folder, created menu and separated game from main

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-import pygame
+from code.Game import Game
 
-print("Setup start")
-pygame.init()
-window = pygame.display.set_mode(size=(800, 600))
-print("Setup end")
-
-print("Main loop start")
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
-    pygame.display.flip()
+game = Game()
+game.run()
